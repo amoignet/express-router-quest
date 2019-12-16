8a77da133e54c3ba2bf6ff721ab7908e489f66ed
Antoine Moignet <antoinemoignet@gmail.com>
2019-12-16 11:14:16 +0100

    feat: quete router sans bonus

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 
-const comments = require('./routes/comments');
-const posts = require('./routes/posts');
-const tags = require('./routes/tags');
+const api = require('./routes');
+
 
 const app = express();
 const port = 8000;
@@ -14,11 +13,9 @@ const port = 8000;
 
 
 //Cette ligne permet de connecter le routeur - et ses routes - au système de routage fourni par app.
-app.use('/api/comments', comments);
+app.use('/api', api);
 
-app.use('/api/posts', posts);
 
-app.use('/api/tags', tags);
 
 app.listen(port, (err) => {
   if (err) {
